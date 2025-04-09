368caada947ce9c716a3b464319b007b642ad717
MustafaAKbulut55 <akbulutmustafa081@gmail.com>
2025-04-09 14:14:25 +0300

    Kamera Düzeltildi
    Kamera Düzeltildi

--- Proje/Assets/Scenes/InGameScene/Scripts/CameraManager.cs
+++ Proje/Assets/Scenes/InGameScene/Scripts/CameraManager.cs
@@ -1,61 +1,32 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using Cinemachine;
 
 public class CameraManager : MonoBehaviour
 {
-    public CinemachineVirtualCamera cmVirtualCam;   // Sanal kamera referansý
-    public Camera mainCamera;                       // Birincil kamera referansý
+    public CinemachineVirtualCamera cmVirtualCam;  // Sahnedeki vcam referansÄ±
+    // Main Camera Ã¼zerinde CinemachineBrain olduÄŸunu varsayÄ±yoruz
+    // AyrÄ± bir "mainCamera" bileÅŸenine genelde ihtiyaÃ§ olmaz.
 
-    private bool usingVirtualCam = true;            // Sanal kamera kullanýlýyor mu?
+    private bool isActive = true;
 
-    // Update is called once per frame
+    void Start()
+    {
+        // Oyun baÅŸlar baÅŸlamaz vcam aktif olsun (priority veya gameObject ile)
+        cmVirtualCam.gameObject.SetActive(true);
+        // Priority'yi de isterseniz yÃ¼kseltebilirsiniz
+        cmVirtualCam.Priority = 10;
+    }
+
     void Update()
     {
-        // Uzay tuþuna basýldýðýnda kamera modunu deðiþtir
+        // Space'e basÄ±ldÄ±ÄŸÄ±nda vcam'i aÃ§/kapa yapmak isterseniz:
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            usingVirtualCam = !usingVirtualCam;  // Kamera modunu tersine çevir
+            isActive = !isActive;
+            cmVirtualCam.gameObject.SetActive(isActive);
 
-            if (usingVirtualCam)
-            {
-                cmVirtualCam.gameObject.SetActive(true);   // Sanal kamerayý etkinleþtir
-                mainCamera.gameObject.SetActive(false);    // Birincil kamerayý devre dýþý býrak
-            }
-            else
-            {
-                cmVirtualCam.gameObject.SetActive(false);  // Sanal kamerayý devre dýþý býrak
-                mainCamera.gameObject.SetActive(true);     // Birincil kamerayý etkinleþtir
-            }
-        }
-
-        // Eðer birincil kamera kullanýlýyorsa, fare pozisyonuna baðlý olarak kamerayý hareket ettir
-        if (!usingVirtualCam)
-        {
-            float x = Input.mousePosition.x;
-            float y = Input.mousePosition.y;
-
-            // Fare ekranýn sol kenarýna yakýnsa, kamerayý sola doðru hareket ettir
-            if (x < 10)
-            {
-                mainCamera.transform.position -= Vector3.left * Time.deltaTime * 10f;
-            }
-            // Fare ekranýn sað kenarýna yakýnsa, kamerayý saða doðru hareket ettir
-            else if (x > Screen.width - 10)
-            {
-                mainCamera.transform.position -= Vector3.right * Time.deltaTime * 10f;
-            }
-            // Fare ekranýn alt kenarýna yakýnsa, kamerayý geriye doðru hareket ettir
-            if (y < 10)
-            {
-                mainCamera.transform.position -= Vector3.back * Time.deltaTime * 10f;
-            }
-            // Fare ekranýn üst kenarýna yakýnsa, kamerayý ileriye doðru hareket ettir
-            else if (y > Screen.height - 10)
-            {
-                mainCamera.transform.position -= Vector3.forward * Time.deltaTime * 10f;
-            }
+            // Dilerseniz Priority'yi de 0/10 arasÄ± deÄŸiÅŸtirebilirsiniz
+            // cmVirtualCam.Priority = isActive ? 10 : 0;
         }
     }
 }
